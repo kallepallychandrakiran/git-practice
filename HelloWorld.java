a36ccc8ecf7a777beb0e0985ac73d5eaa5e3c399
chandrakiran <kallepallychandrakiran@gmail.com>
2024-07-07 22:05:10 +0530

    feature-2 added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
-class HelloWorld{
-	public static void main(String[]args){
+class HelloWorld {
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Added new feature from feature branch");
+		System.out.println("Added new feature from feature branch 2");
 	}
 }
